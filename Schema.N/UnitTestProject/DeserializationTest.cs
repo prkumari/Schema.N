2b672df87aefc1d0003b6433ebfcd1700356da27
dachryst <dachryst@microsoft.com>
2017-07-26 10:22:46 -0700

    fix broken test

--- Schema.N/UnitTestProject/DeserializationTest.cs
+++ Schema.N/UnitTestProject/DeserializationTest.cs
@@ -14,51 +14,45 @@ namespace UnitTestProject
         [TestMethod]
         public void VersionDeserializeTest()
         {
-            //var jsonV1Text = File.ReadAllText(@"C:\hackathon\Schema.N\Schema.N\TestProject\PV1.txt");
-            //var jsonV2Text = File.ReadAllText(@"C:\hackathon\Schema.N\Schema.N\TestProject\PV2.txt");
+			var jsonV1Text = File.ReadAllText(@"PV1.txt");
+			var jsonV2Text = File.ReadAllText(@"PV2.txt");
 
-            //var jtokenV1 = JObject.Parse(jsonV1Text);
-            //var jtokenV2 = JObject.Parse(jsonV2Text);
+			var jtokenV1 = JObject.Parse(jsonV1Text);
+			var jtokenV2 = JObject.Parse(jsonV2Text);
 
-            //var entityConversion = new JsonToEntityConversion();
+			var entityConversion = new JsonToEntityConversion();
 
-            //var entityVersion1Matcher = new VersionMatcher
-            //{
-            //    EntityVersion = 1,
-            //    EntityMatchesFunc = jObject => jObject.Value<int>("SchemanVersion") == 1,
-            //    Weight = 1
-            //};
+			var entityVersion1Matcher = new VersionMatcher
+			{
+				EntityVersion = 1,
+				EntityMatchesFunc = jObject => jObject.Value<int>("SchemanVersion") == 1,
+				Weight = 1
+			};
 
-            //var entityVersion2Matcher = new VersionMatcher
-            //{
-            //    EntityVersion = 2,
-            //    EntityMatchesFunc = jObject => jObject.Value<int>("SchemanVersion") == 2,
-            //    Weight = 2
-            //};
+			var entityVersion2Matcher = new VersionMatcher
+			{
+				EntityVersion = 2,
+				EntityMatchesFunc = jObject => jObject.Value<int>("SchemanVersion") == 2,
+				Weight = 2
+			};
 
-            //var versionDetector = new CompositeVersionDetector(new List<IVersionMatcher> { entityVersion1Matcher, entityVersion2Matcher });
+			var version1Info = new NewPocoVersionInfo<PersonV1>(1, null, entityVersion1Matcher);
+			var version2Info = new NewPocoVersionInfo<PersonV2>(2, null, entityVersion2Matcher);
+			entityConversion.RegisterNewVersion(version1Info);
+			entityConversion.RegisterNewVersion(version2Info);
 
-            ////entityConversion.VersionDetector = new DefaultEntityVersionDetector("SchemanVersion");
-            ////entityConversion.RegisterDeserializer("1", new DefaultEntityVersionDeserialization<PersonV1>());
-            ////entityConversion.RegisterDeserializer("2", new DefaultEntityVersionDeserialization<PersonV2>());
+			var v1Entity = entityConversion.DeserializeJsonToCurrentVersion(jtokenV1);
+			var v2Entity = entityConversion.DeserializeJsonToCurrentVersion(jtokenV2);
 
-            //entityConversion.SetVersionDetector(versionDetector);
-            //entityConversion.RegisterDefaultDeserializer<PersonV1>(1);
-            //entityConversion.RegisterDefaultDeserializer<PersonV2>(2);
+			Assert.AreEqual(v1Entity.EntityType, typeof(PersonV1));
+			Assert.AreEqual(v2Entity.EntityType, typeof(PersonV2));
+		}
 
-            //var v1Entity = entityConversion.DeserializeJsonToCurrentVersion(jtokenV1);
-            //var v2Entity = entityConversion.DeserializeJsonToCurrentVersion(jtokenV2);
-
-            //Assert.AreEqual(v1Entity.EntityType, typeof (PersonV1));
-            //Assert.AreEqual(v2Entity.EntityType, typeof (PersonV2));
-        }
-
-        [TestMethod]
+		[TestMethod]
         public void ConvertValidateBasicOps()
         {
             var jsonV1Text = File.ReadAllText(@"PV1.txt");
             var jsonV2Text = File.ReadAllText(@"PV2.txt");
-
 
 	        var r1 = new JsonTransformRule("Name", JsonTransformRuleType.Rename, "awesome");
 	        var r2 = new JsonTransformRule("awesome", JsonTransformRuleType.CopyToken, "SchemanVersion");
