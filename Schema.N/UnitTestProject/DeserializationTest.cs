cd9bb32cf4bd5007746f62cdbb20202bc0268b65
prkumari <30417984+prkumari@users.noreply.github.com>
2017-07-25 17:33:52 -0700

    Add more unit test

--- Schema.N/UnitTestProject/DeserializationTest.cs
+++ Schema.N/UnitTestProject/DeserializationTest.cs
@@ -83,9 +83,36 @@ namespace UnitTestProject
             rules.Add(r2);
             rules.Add(r3);
             var result = jc.ConvertTo(jsonV1Text, jsonV2Text, rules);
-            Assert.AreEqual("{\r\n  \"Id\": 1,\r\n  \"FirstName\": \"Priya\",\r\n  \"LastName\": " +
+            Assert.AreEqual("{\r\n  \"Identifier\": 1,\r\n  \"FirstName\": \"Priya\",\r\n  \"LastName\": " +
                 "\"Kumari\",\r\n  \"DoB\": \"1989-02-01\",\r\n  \"SchemanVersion\": 1,\r\n  \"Example\": " +
                 "\"Priya Kumari\"\r\n}", result);
         }
+
+        [TestMethod]
+        public void ConvertValidateVersionUpgrade()
+        {
+            var jsonV1Text = File.ReadAllText(@"UserV1.txt");
+            var jsonV2Text = File.ReadAllText(@"UserV2.txt");
+
+            var jc = new JsonTransformer();
+            var r1 = new JsonTransformRule()
+            {
+                Operation = JsonTransformRuleType.Rename,
+                TargetPath = "about",
+                Value = "description"
+            };
+            var r3 = new JsonTransformRule()
+            {
+                Operation = JsonTransformRuleType.Delete,
+                TargetPath = "eyeColor",
+            };
+
+            var rules = new List<JsonTransformRule>();
+            rules.Add(r1);
+            rules.Add(r3);
+            var result = jc.ConvertTo(jsonV1Text, jsonV2Text, rules);
+            Assert.AreEqual("{\r\n  \"guid\": \"6557159b-84e8-4651-a6b2-bc57f84c1a7e\",\r\n  \"isActive\": true,\r\n  \"age\": 20,\r\n  \"name\": \"Singleton Craft\",\r\n  \"gender\": \"male\",\r\n  \"address\": \"413 Forrest Street, Lemoyne, Kentucky, 381\",\r\n  \"phone\": \"+1 (971) 407-3748\",\r\n  \"description\": \"Occaecat exercitation consectetur do anim magna nisi sunt. Dolore Lorem ea fugiat velit reprehenderit laboris incididunt cupidatat occaecat velit. Et aliqua nisi nisi amet et velit quis commodo culpa exercitation ad. Labore consectetur ullamco non aliqua ullamco sit do consequat esse nisi labore fugiat. Reprehenderit laboris esse qui laboris amet sint aliquip commodo.\\r\\n\"\r\n}", result);
+        }
+
     }
 }
